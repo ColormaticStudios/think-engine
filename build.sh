2c45fba81f149023c2032ceed26a99a8b2bd7391
Zakarya <zakaryacarroll.contact@gmail.com>
2022-08-05 15:30:19 -0700

    Created a way to fancy build script

--- build.sh
+++ build.sh
@@ -1,12 +1,27 @@
-echo "building thinking engine..."
+#!/bin/bash
+# tputcolors
 
 
-mkdir bin
-crystal build src/main.cr -o bin/think
+if [ -n "$1" ]; then
+  if [ "$1" == "-r" ]; then
+    mode="release"
+  else
+    if [ "$1" == "-d" ]; then
+      mode="debug"
+    fi
+  fi
+  echo "building thinking engine..."
+
+  mkdir bin
+  crystal build src/main.cr -o bin/think --$mode
 
 
-if [ $? = 0 ]; then
-   echo "build successful."
+  if [ "$?" == 0 ]; then
+     echo "build successful."
+  else
+     echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) build failed with exit code $?"
+  fi
+
 else
-   echo "build failed with exit code $?"
+  echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) you did not specify a build mode. Either add -r for release or -d for debug."
 fi
