3aa0d2212667178c80c059de5fba295714ab6d64
Zakarya <zakaryacarroll.contact@gmail.com>
2022-08-05 15:43:57 -0700

    added comments to the build script

--- build.sh
+++ build.sh
@@ -2,23 +2,26 @@
 # tputcolors
 
 
-if [ -n "$1" ]; then
+if [ -n "$1" ]; then #test if there is an argument
+  #if there is, test what it is.
   if [ "$1" == "-r" ]; then
     mode="release"
   else
     if [ "$1" == "-d" ]; then
       mode="debug"
+    else
+      echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) you have given an incorrect argument. The build process will attempt to continue."
     fi
   fi
   echo "building thinking engine..."
 
-  if [ ! -d "./bin" ]; then
-    mkdir bin
+  if [ ! -d "./bin" ]; then #test if the bin file exists
+    mkdir bin #if not, make it
   fi
-  crystal build src/main.cr -o bin/think --error-trace --$mode
+  crystal build src/main.cr -o bin/think --error-trace --$mode #actually build the program with the build options
 
 
-  if [ "$?" == 0 ]; then
+  if [ "$?" == 0 ]; then #test if the build was successful
      echo "build successful."
   else
      echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) build failed with exit code $?"
