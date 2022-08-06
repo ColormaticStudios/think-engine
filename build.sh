65635fd1ef9b25289b53dee1459bbcc2666bb850
Zakarya <zakaryacarroll.contact@gmail.com>
2022-08-05 21:03:01 -0700

    tweak colors of build script

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if [ -n "$1" ]; then #test if there is an argument
       echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) you have given an incorrect argument. The build process will attempt to continue."
     fi
   fi
-  echo "$(tput setaf 2)building thinking engine...$(tput sgr0)"
+  echo "$(tput setaf 3)building thinking engine...$(tput sgr0)"
 
   if [ ! -d "./bin" ]; then #test if the bin file exists
     mkdir bin #if not, make it
@@ -22,7 +22,7 @@ if [ -n "$1" ]; then #test if there is an argument
 
 
   if [ "$?" == 0 ]; then #test if the build was successful
-     echo "build successful."
+     echo "$(tput setaf 10)build successful.$(tput sgr0)"
   else
      echo "$(tput setaf 1)$(tput bold)ERROR:$(tput sgr0) build failed with exit code $?"
   fi
