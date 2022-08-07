2c0d999a400b3410aca7a1aca255133038b4ba63
Zakarya <zakaryacarroll.contact@gmail.com>
2022-08-06 17:05:52 -0700

    make the load_module function one line

--- src/module/module.cr
+++ src/module/module.cr
@@ -3,7 +3,6 @@ require "lua"
 
 class Module
   def load_module(module_to_load)
-    file_to_run = File.read("#{Path.home}/.local/share/think/modules/#{module_to_load}/main.lua")
-    Lua.run file_to_run
+    Lua.run File.read("#{Path.home}/.local/share/think/modules/#{module_to_load}/main.lua")
   end
 end
